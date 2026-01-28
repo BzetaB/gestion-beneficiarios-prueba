30c2a2921b5ba466578a125e5692d0464eb88f44
bzetab <zevallosbenjamin100@gmail.com>
2026-01-28 12:34:11 -0500

    add DTO for sp_Beneficiary_GetDocumentNumbers

---
+++ gestion-beneficiarios/DTOs/BeneficiaryDocumentDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace gestion_beneficiarios.DTOs
+{
+    public class BeneficiaryDocumentDTO
+    {
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string DocumentNumber { get; set; } = null!;
+
+        public string IdentityDocument { get; set; } = null!;
+
+        public bool IsActive { get; set; }
+    }
+}
