add0e6a634a93344493825c2ef2dc404726444d1
bzetab <zevallosbenjamin100@gmail.com>
2026-01-28 12:27:42 -0500

    add DbSets for domain entities in AppDbContext

---
+++ gestion-beneficiarios/Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using gestion_beneficiarios.DTOs;
+using gestion_beneficiarios.Models;
+using Microsoft.EntityFrameworkCore;
+namespace gestion_beneficiarios.Context
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Beneficiary> Beneficiaries { get; set; }
+        public DbSet<IdentityDocument> IdentityDocuments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IdentityDocument>()
+                .ToTable("IdentityDocument");
+
+            modelBuilder.Entity<Beneficiary>()
+                .ToTable("Beneficiary");
+
+            modelBuilder.Entity<BeneficiaryDocumentDTO>().HasNoKey();
+        }
+    }
+}
